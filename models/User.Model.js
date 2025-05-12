04ca55869175c68db02f931ff277e119205ff0c5
Mohd Irfan <mohdirfan70097@gmail.com>
2025-05-12 23:09:22 +0530

    index email or username

--- models/User.Model.js
+++ models/User.Model.js
@@ -96,7 +96,7 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Indexes
-UserSchema.index({ email: 1 });
+UserSchema.index({ email: 1 }, { unique: true });
 UserSchema.index({ username: 1 });
 
 // Virtuals, methods, or statics can go here (e.g., for password hashing, checking block status, etc.)
